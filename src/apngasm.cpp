089a055dfb3a2a6fca0dbaaa2407a3c3bc6797da
影月 零 <zero@genshin.org>
2013-07-08 13:00:40 +0900

    initial sources

---
+++ src/apngasm.cpp
@@ -0,0 +1,9 @@
+#include "libapngasm.h"
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "Initializing apngasm v2" << endl;
+	return 0;
+}
---
+++ src/libapngasm.h
@@ -0,0 +1,5 @@
+#ifndef _LIBAPNGASM_H_
+#define _LIBAPNGASM_H_
+
+
+#endif /* _LIBAPNGASM_H_ */
