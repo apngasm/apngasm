5568e324ba8b6156951913c0ee35191967ee6d5c
kou <kou@genshin.org>
2014-05-14 15:22:02 +0900

    Implements APNGAsm::addFrame method for rgb and rgba.

--- lib/src/apngasm.cpp
+++ lib/src/apngasm.cpp
@@ -235,6 +235,26 @@ namespace apngasm {
   }
 
   //Adds an APNGFrame object to the frame vector
+  //Returns the frame number in the frame vector
+  size_t APNGAsm::addFrame(rgb *pixels, unsigned int width, unsigned int height, rgb *trns_color, unsigned delayNum, unsigned delayDen)
+  {
+    _frames.push_back(
+      APNGFrame(pixels, width, height, trns_color, delayNum, delayDen)
+    );
+    return _frames.size();
+  }
+
+  //Adds an APNGFrame object to the frame vector
+  //Returns the frame number in the frame vector
+  size_t APNGAsm::addFrame(rgba *pixels, unsigned int width, unsigned int height, unsigned delayNum, unsigned delayDen)
+  {
+    _frames.push_back(
+      APNGFrame(pixels, width, height, delayNum, delayDen)
+    );
+    return _frames.size();
+  }
+
+  //Adds an APNGFrame object to the frame vector
   //Returns a frame vector with the added frames
   APNGAsm& APNGAsm::operator << (const APNGFrame &frame)
   {
