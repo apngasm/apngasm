949dee3d19564eaacca3aeb99d4843dcb615da5a
kou <kou@genshin.org>
2016-11-15 14:38:19 +0900

    Copy native libraries when after ndk build.

--- lib/interfaces/android/androidbuild.rb
+++ lib/interfaces/android/androidbuild.rb
@@ -8,18 +8,9 @@ require 'fileutils'
 @natives_dir = ARGV[2] || "#{@source_dir}/tmp/build/"
 @interface_dir = "#{@build_dir}/japngasm/"
 
-puts "== copying natives"
-FileUtils.cp_r("#{@source_dir}/interfaces/android/jni", @build_dir, {remove_destination: true})
-FileUtils.mkdir_p(@interface_dir)
-FileUtils.mkdir_p("#{@build_dir}/libs")
-FileUtils.mkdir_p("#{@build_dir}/libs/armeabi")
-FileUtils.cp_r(Dir["#{@natives_dir}/natives/lib/arm/*.so*"].collect{|f| File.expand_path(f)}, "#{@build_dir}/libs/armeabi/", {remove_destination: true})
-FileUtils.mkdir_p("#{@build_dir}/libs/x86")
-FileUtils.cp_r(Dir["#{@natives_dir}/natives/lib/x86/*.so*"].collect{|f| File.expand_path(f)}, "#{@build_dir}/libs/x86/", {remove_destination: true})
-FileUtils.mkdir_p("#{@build_dir}/libs/mips")
-FileUtils.cp_r(Dir["#{@natives_dir}/natives/lib/mips/*.so*"].collect{|f| File.expand_path(f)}, "#{@build_dir}/libs/mips/", {remove_destination: true})
-
 puts "== Copying NDK build files and modifying variables"
+FileUtils.cp_r("#{@source_dir}/interfaces/android/jni", @build_dir, {remove_destination: true})
+
 txt = File.read("#{@build_dir}/jni/Android.mk")
 
 # set source path
@@ -38,6 +29,16 @@ puts "== Creating Native Interface Java sources"
 
 puts "== Running NDK Build"
 `NDK_PROJECT_PATH=#{@build_dir} #{ENV["CRYSTAX_NDK"]}/ndk-build`
+
+puts "== copying natives"
+FileUtils.mkdir_p(@interface_dir)
+FileUtils.mkdir_p("#{@build_dir}/libs")
+FileUtils.mkdir_p("#{@build_dir}/libs/armeabi")
+FileUtils.cp_r(Dir["#{@natives_dir}/natives/lib/arm/*.so*"].collect{|f| File.expand_path(f)}, "#{@build_dir}/libs/armeabi/", {remove_destination: true})
+FileUtils.mkdir_p("#{@build_dir}/libs/x86")
+FileUtils.cp_r(Dir["#{@natives_dir}/natives/lib/x86/*.so*"].collect{|f| File.expand_path(f)}, "#{@build_dir}/libs/x86/", {remove_destination: true})
+FileUtils.mkdir_p("#{@build_dir}/libs/mips")
+FileUtils.cp_r(Dir["#{@natives_dir}/natives/lib/mips/*.so*"].collect{|f| File.expand_path(f)}, "#{@build_dir}/libs/mips/", {remove_destination: true})
 
 puts "== for mac (copy the missing file)"
 
