ac2816096623d9e372e26fb97b5a8ad60c7a83c0
kou <kou@genshin.org>
2018-06-13 17:37:14 +0900

    Update android build for ARMv8.

--- lib/interfaces/android/jni/Application.mk
+++ lib/interfaces/android/jni/Application.mk
@@ -2,4 +2,4 @@ NDK_TOOLCHAIN_VERSION := 5
 APP_STL := gnustl_shared
 APP_CPPFLAGS += -std=gnu++11
 APP_ABI := armeabi armeabi-v7a x86 arm64-v8a x86_64
-APP_PLATFORM = android-21
+APP_PLATFORM = android-16
