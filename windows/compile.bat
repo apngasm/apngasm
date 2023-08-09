bd6879d06addc8b8bdabe30a343f5ab022dcbd1a
laggykiller <61652821+laggykiller@users.noreply.github.com>
2023-08-09 22:27:07 +0800

    Fix build

--- windows/compile.bat
+++ windows/compile.bat
@@ -2,5 +2,5 @@
 
 mkdir ..\release
 
-cmake -DBUILD_SHARED_LIBS=OFF -DZLIB_ROOT=%cd%\windows\zlib -DPNG_ROOT=%cd%\windows\libpng -DBOOST_ROOT=%cd%\windows\boost -DCMAKE_INSTALL_PREFIX:PATH=%cd%\..\release ..
+cmake -DBUILD_SHARED_LIBS=OFF -DZLIB_ROOT=%cd%\zlib -DPNG_ROOT=%cd%\libpng -DBOOST_ROOT=%cd%\boost -DCMAKE_INSTALL_PREFIX:PATH=%cd%\..\release ..
 cmake --build . --config Release --target INSTALL
